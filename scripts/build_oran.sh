aa94cc12c1af3a928e60026f89b9276b26b512e9
Jackie Huang <jackie.huang@windriver.com>
2020-03-02 19:29:01 +0800

    build_oran.sh: add support for nxp-lx2xxx
    - remove the default templates
    - add templates according to different BSP
    - ceph is only added for x86-64
    - linux-yocto-rt: add support for nxp-lx2xxx
    - add patch for nxp-lx2xxx to support preempt-rt
    - add an option for build script to select BSP
    
    Issue-ID: INF-7
    Signed-off-by: Jackie Huang <jackie.huang@windriver.com>
    Change-Id: I80c8acd9745db7003c56594ee36a1643ac269f2c

--- scripts/build_oran.sh
+++ scripts/build_oran.sh
@@ -17,12 +17,24 @@
 # Ensure we fail the job if any steps fail.
 set -e -o pipefail
 
+SUPPORTED_BSP="intel-x86-64 nxp-lx2xxx"
+WRTEMPLATE_COMMON="feature/oran-host-rt-tune feature/kubernetes feature/self-hosted"
+WRTEMPLATE_X86="feature/hosts-ia feature/kvm feature/dpdk"
+
+SRC_ORAN_BRANCH="master"
+SRC_WRL_BRANCH="WRLINUX_10_18_BASE"
+SRC_WRL_URL="git://github.com/WindRiver-Labs/wrlinux-x.git"
+
+SCRIPTS_DIR=$(dirname $(readlink -f $0))
+
 help_info () {
 cat << ENDHELP
 Usage:
-$(basename $0) <-w WORKSPACE_DIR> [-n] [-h]
+$(basename $0) <-w WORKSPACE_DIR> [-b BSP] [-n] [-h] [-r]
 where:
     -w WORKSPACE_DIR is the path for the project
+    -b BPS is one of supported BSP: "${SUPPORTED_BSP}"
+       (default is intel-x86-64 if not specified.)
     -n dry-run only for bitbake
     -h this help info
     -e EXTRA_CONF is the pat for extra config file
@@ -67,18 +79,33 @@ check_yn_rm_work () {
     esac
 }
 
+check_valid_bsp () {
+    bsp="$1"
+    for b in ${SUPPORTED_BSP}; do
+        if [ "${bsp}" == "${b}" ]; then
+            BSP_VALID="${bsp}"
+            break
+        fi
+    done
+    if [ -z "${BSP_VALID}" ]; then
+        echo_error "${bsp} is not a supported BSP, the supported BSPs are: ${SUPPORTED_BSP}"
+        exit 1
+    fi
+}
+
 DRYRUN=""
 EXTRA_CONF=""
 RM_WORK="Yes"
+BSP="intel-x86-64"
 
-SCRIPTS_DIR=`dirname $0`
-SCRIPTS_DIR=`readlink -f $SCRIPTS_DIR`
-
-while getopts "w:e:r:nh" OPTION; do
+while getopts "w:b:e:r:nh" OPTION; do
     case ${OPTION} in
         w)
             WORKSPACE=`readlink -f ${OPTARG}`
             ;;
+        b)
+	    check_valid_bsp ${OPTARG}
+	    ;;
         e)
             EXTRA_CONF=`readlink -f ${OPTARG}`
             ;;
@@ -95,29 +122,33 @@ while getopts "w:e:r:nh" OPTION; do
     esac
 done
 
-if [ -z ${WORKSPACE} ]; then
+if [ -z "${WORKSPACE}" ]; then
     echo_info "No workspace specified, a directory 'workspace' will be created in current directory as the workspace"
     WORKSPACE=`readlink -f workspace`
 fi
 
-SRC_WRL_DIR=${WORKSPACE}/src_wrl1018
+if [ -n "${BSP_VALID}" ]; then
+    BSP="${BSP_VALID}"
+fi
+
+SRC_WRL_DIR=${WORKSPACE}/src_wrlinux
 SRC_ORAN_DIR=${WORKSPACE}/src_oran
 PRJ_BUILD_DIR=${WORKSPACE}/prj_oran-inf
 
 mkdir -p ${SRC_WRL_DIR} ${PRJ_BUILD_DIR} ${SRC_ORAN_DIR}
 
 echo_info "The following directories are created in your workspace(${WORKSPACE}):"
-echo_info "For wrlinux1018 source: ${SRC_WRL_DIR}"
+echo_info "For wrlinux source: ${SRC_WRL_DIR}"
 echo_info "For oran layer source: ${SRC_ORAN_DIR}"
 echo_info "For build project: ${PRJ_BUILD_DIR}"
 
-# Clone the source of WRLinux BASE 10.18 from github and setup
-RUN_CMD="git clone --branch WRLINUX_10_18_BASE git://github.com/WindRiver-Labs/wrlinux-x.git"
-echo_cmd "Cloning wrlinux 1018 source from github:"
+# Clone the source of WRLinux from github and setup
+RUN_CMD="git clone --branch ${SRC_WRL_BRANCH} ${SRC_WRL_URL}"
+echo_cmd "Cloning wrlinux-x source from github:"
 cd ${SRC_WRL_DIR}
 ${RUN_CMD}
 
-RUN_CMD="./wrlinux-x/setup.sh --machines intel-x86-64 --layers meta-cloud-services"
+RUN_CMD="./wrlinux-x/setup.sh --machines ${BSP} --layers meta-cloud-services"
 echo_cmd "Setup wrlinux build project:"
 ${RUN_CMD}
 
@@ -133,9 +164,15 @@ if cd ${SCRIPTS_DIR} && git rev-parse --is-inside-work-tree > /dev/null 2>&1; th
 else
     echo_info "Cloning oran layer:"
     cd ${SRC_ORAN_DIR}
-    RUN_CMD="git clone https://gerrit.o-ran-sc.org/r/pti/rtp"
+    RUN_CMD="git clone --branch ${SRC_ORAN_BRANCH} https://gerrit.o-ran-sc.org/r/pti/rtp"
     echo_cmd "Cloing with:"
     ${RUN_CMD}
+fi
+
+# Apply meta patches for nxp-lx2xxx
+if [ "${BSP}" == "nxp-lx2xxx" ]; then
+    cd ${SRC_WRL_DIR}/layers/nxp-lx2xxx/
+    git am ${SRC_ORAN_DIR}/rtp/scripts/meta-patches/nxp-lx2xxx/0001-Revert-nxp-lx2xxx-remove-preempt-rt-related-file.patch
 fi
 
 # Source the build env
@@ -157,10 +194,20 @@ cat << EOF >> conf/local.conf
 ########################
 DISTRO = "oran-inf"
 BB_NO_NETWORK = '0'
-WRTEMPLATE += "feature/oran-host-rt-tune"
 
 # Work around for CI build
 IMAGE_INSTALL_remove = "ceph"
+
+# For container image
+WR_APP_CONTAINER_APP = "rt-tests"
+
+# Disable multilib fo nxp-lx2xxx
+MULTILIBS_nxp-lx2xxx = ""
+DEFAULTTUNE_virtclass-multilib-lib32_nxp-lx2xxx = ""
+
+# Add templates
+WRTEMPLATE += "${WRTEMPLATE_COMMON}"
+WRTEMPLATE_prepend_x86-64 += "${WRTEMPLATE_X86}"
 EOF
 
 if [ "${RM_WORK}" == "Yes" ]; then
@@ -177,5 +224,9 @@ TIMESTAMP=`date +"%Y%m%d_%H%M%S"`
 RUN_CMD="bitbake ${DRYRUN} oran-image-inf-host"
 echo_cmd "Build the oran-image-inf-host image"
 bitbake ${DRYRUN} oran-image-inf-host 2>&1|tee logs/bitbake_oran-image-inf-host_${TIMESTAMP}.log
+
+RUN_CMD="bitbake ${DRYRUN} wr-app-container"
+echo_cmd "Build the wr-app-container image"
+bitbake ${DRYRUN} wr-app-container 2>&1|tee logs/bitbake_wr-app-container_${TIMESTAMP}.log
 
 echo_info "Build succeeded, you can get the image in ${PRJ_BUILD_DIR}/tmp-glibc/deploy/images/intel-x86-64/oran-image-inf-host-intel-x86-64.iso"
